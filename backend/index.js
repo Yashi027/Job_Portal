007232f34eb3846cb6f477c772ab1e9ad49fb7cf
Yashi Bansal <bansalyashi163@gmail.com>
2026-02-05 22:25:43 +0530

    Fix nested repo issue

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+const app = express();
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config({});
+
+app.get("/",(req,res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Backend running"
+    })
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+    connectDB()
+})
